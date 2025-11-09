6e47229872c22cba4f50bbc82a5e1596ddf1f645
root <root@srv773635>
2025-11-09 14:38:29 +0000

    Auto-update: AI model

--- AI_Modeler_Demo_01.py
+++ AI_Modeler_Demo_01.py
@@ -1,3 +1,15 @@
 !ls /repos
 
 !cd /repos
+
+import os
+import openai
+import tiktoken
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+LLM_MODEL="gpt-4o"
+
+TOKENIZER = tiktoken.encoding_for_model("text-embedding-3-small")
+
+EMBED_MODEL="text-embedding-3-small"
